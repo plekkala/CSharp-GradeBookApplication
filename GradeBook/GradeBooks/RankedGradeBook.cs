123def54ac160f4a5f205b8b14056acb7323ac85
Pradeep Lekkala <PradeepL@AICORPORATION.COM>
2020-10-17 22:45:15 +0100

    lesson3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,6 +13,44 @@ namespace GradeBook.GradeBooks
         {
             this.Type = GradeBookType.Ranked;
         }
-       
+
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int j = 0;
+            foreach (var student in Students)
+            {
+                
+                if(averageGrade < student.AverageGrade)
+                {
+                    j++;
+                }
+                
+            }
+
+            
+            double rank = (j * 100)/ Students.Count;
+
+
+
+
+
+            if ( rank >= 80)
+                return 'A';
+            else if (rank >= 60)
+                return 'B';
+            else if (rank >= 40)
+                return 'C';
+            else if (rank >= 20)
+                return 'D';
+            else
+                return 'F';
+        }
+
     }
 }
