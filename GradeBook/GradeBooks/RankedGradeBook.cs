8d5d326a14f892339e3e6182b025b84c3ebb0f77
Pradeep Lekkala <PradeepL@AICORPORATION.COM>
2020-10-17 12:09:54 +0100

    intial commit

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,18 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+
+        public GradeBookType Type { get; set; }
+        public RankedGradeBook(string name) :base(name)
+        {
+            this.Type = GradeBookType.Standard;
+        }
+       
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,20 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        //public GradeBookType Type;
+        public StandardGradeBook(string name) :base(name)
+        {
+            Name = name;
+            Type = GradeBookType.Standard;
+        }
+
+
+        
+    }
+}
